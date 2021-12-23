ecbf483ce0e0ac5060889e2821f5676d8b4f2a33
Luzon555 <96468897+Luzon555@users.noreply.github.com>
2021-12-23 10:44:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to Luzon app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          my first page in <code> netlify.com </code>
         </p>
       </main>
 
